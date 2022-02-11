45689dd69c266c96c25e6cb8e47da47ea9b50147
James Helms <jbhelms@gmail.com>
2022-02-11 10:38:13 -0800

    Requirement 4: make negative numbers throw an exception made this configurable updated unit tests and program to test

--- challenge-calculator/Restaurant365.App/Program.cs
+++ challenge-calculator/Restaurant365.App/Program.cs
@@ -19,6 +19,8 @@ namespace Restaurant365.App
             calculator.Delimiters.Add("\n");
             calculator.Delimiters.Add("abc");
 
+            calculator.DenyNegativeNumber = true;
+
             ///Windows closes console apps when you press ctrl-c
             while (true)
             {
